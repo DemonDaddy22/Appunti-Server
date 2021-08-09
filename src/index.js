6c0660827185bdad76a5a8ee400386346a336444
Rohan Gupta <guptarohan369@gmail.com>
2021-08-09 23:56:06 +0530

    Create mongoose connection to appunti db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 /* eslint-disable no-unused-vars */
+import dotenv from 'dotenv';
+dotenv.config();
+
 import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
 
 import BookSearchRoutes from './routes/Books/BookSearch';
 
 const app = express();
 const port = process.env.PORT || 5000;
+const dbUrl = process.env.dbUrl || 'mongodb://localhost:27017/appunti';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err));
+db.once('open', () => console.log('> Database connection established'));
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/api/v1/books', BookSearchRoutes);
 
