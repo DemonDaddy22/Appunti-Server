346b4d40fe490052a387c09bf3cd96e55ed72c8f
Rohan Gupta <guptarohan369@gmail.com>
2021-08-22 00:12:18 +0530

    Create Book and BookShelf models

---
+++ src/models/Book.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    _id: String,
+    gid: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    publishedDate: {
+        type: String,
+        required: true,
+    },
+    pageCount: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    ratingsCount: {
+        type: Number,
+        required: true,
+    },
+    language: {
+        type: String,
+    },
+    imageLink: {
+        type: String,
+        required: true,
+    },
+    epub: {
+        isAvailable: Boolean,
+        acsTokenLink: String,
+    },
+    pdf: {
+        isAvailable: Boolean,
+        acsTokenLink: String,
+    },
+    authors: [String],
+    categories: [String],
+    industryIdentifiers: [
+        {
+            identifier: String,
+            type: String,
+        }
+    ]
+});
+
+const Book = mongoose.model('book', BookSchema);
+
+export default Book;
---
+++ src/models/BookShelf.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const BookShelfSchema = new Schema({
+    _id: String,
+    userId: {
+        type: String,
+        required: true,
+    },
+    books: {
+        type: [String],
+        maxLength: 30,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    coverImageLink: {
+        type: String,
+    },
+});
+
+const BookShelf = mongoose.model('bookshelf', BookShelfSchema);
+
+export default BookShelf;
